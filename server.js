dd4b2d1f8b17f7a5d38c7c8936c1ba93f1772ff8
unknown <asiazmudziak@gmail.com>
2021-05-19 22:36:31 +0200

    selecting database

--- server.js
+++ server.js
@@ -15,22 +15,26 @@ mongoClient.connect(
       console.log(err);
     } else {
       console.log('Successfully connected to the database');
+
+      const db = client.db('companyDB');
+      
+      const app = express();
+
+      app.use(cors());
+      app.use(express.json());
+      app.use(express.urlencoded({ extended: false }));
+
+      app.use('/api', employeesRoutes);
+      app.use('/api', departmentsRoutes);
+      app.use('/api', productsRoutes);
+
+      app.use((req, res) => {
+        res.status(404).send({ message: 'Not found...' });
+      });
+
+      app.listen('8000', () => {
+        console.log('Server is running on port: 8000');
+      });
     }
   }
 );
-
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-app.use('/api', employeesRoutes);
-app.use('/api', departmentsRoutes);
-app.use('/api', productsRoutes);
-
-app.use((req, res) => {
-  res.status(404).send({ message: 'Not found...' });
-})
-
-app.listen('8000', () => {
-  console.log('Server is running on port: 8000');
-});
